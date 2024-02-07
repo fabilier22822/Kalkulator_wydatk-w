5f20358f4206d9480e99eea77609580d3912e053
Jakub Kunikowski <j.kunikowski.165@studms.ug.edu.pl>
2024-02-07 10:53:04 +0100

    pierwsze okienko

--- apka.py
+++ apka.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text="Hello World", font_size=21)
+
+if __name__ == '__main__':
+    MyApp().run()
